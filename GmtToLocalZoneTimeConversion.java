d374e3fccd8288fee63c32d1f0d9ae15ec1d2d4a
vmanoj003 <36049483+vmanoj003@users.noreply.github.com>
2019-05-22 00:54:31 -0400

    inital code Checkin

---
+++ GmtToLocalZoneTimeConversion.java
@@ -0,0 +1,35 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class GmtToLocalZoneTimeConversion {
+
+    private static final String DATE_FORMAT = "dd-MM-yyyy'T'kk:mm:ss";
+
+    public static void main(String[] args) throws ParseException {
+
+
+       // SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+
+        String dateInString = "22-01-2015T10:15:55";
+
+        LocalDateTime ldt = LocalDateTime.parse(dateInString, formatter.ofPattern(DATE_FORMAT));
+
+        ZoneId singaporeZoneId = ZoneId.of("Asia/Singapore");
+        System.out.println("TimeZone : " + singaporeZoneId);
+
+        ZonedDateTime asiaZonedDateTime = ldt.atZone(singaporeZoneId);
+        System.out.println("Date (Singapore) : " + asiaZonedDateTime);
+
+        System.out.println("\n---DateTimeFormatter---");
+        System.out.println("Date (Singapore) : " + formatter.format(asiaZonedDateTime));
+
+
+
+
+    }
+}
